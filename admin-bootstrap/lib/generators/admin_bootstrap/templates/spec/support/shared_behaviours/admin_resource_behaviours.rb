cc368fe3b59fe07f0cc489501d05b30dbb5181ac
Igor Rzegocki <ajgon@irgon.com>
2012-08-10 14:51:22 +0200

    Fixed tests for generated controllers

--- admin-bootstrap/lib/generators/admin_bootstrap/templates/spec/support/shared_behaviours/admin_resource_behaviours.rb
+++ admin-bootstrap/lib/generators/admin_bootstrap/templates/spec/support/shared_behaviours/admin_resource_behaviours.rb
@@ -51,15 +51,13 @@ shared_examples_for "admin resource" do
       {}
     else
       field_with_presence_validation = field_with_presence_validation.attributes.first
-      binary_columns = model.columns.find_all {|c| c.type == :binary}.collect {|c| c.name.to_s}
-      build(model.to_s.underscore.to_sym, field_with_presence_validation => '').attributes.except(*(['id', 'created_at', 'updated_at'] + binary_columns))
+      build(model.to_s.underscore.to_sym, field_with_presence_validation => '').attributes.slice(*(model.accessible_attributes.to_a))
     end
   end
 
   def correct_data
     (model = described_class.extract_resource).should be_a_descendant_of(ActiveRecord::Base)
-    binary_columns = model.columns.find_all {|c| c.type == :binary}.collect {|c| c.name.to_s}
-    build(model.to_s.underscore.to_sym).attributes.except(*(['id', 'created_at', 'updated_at'] + binary_columns))
+    build(model.to_s.underscore.to_sym).attributes.slice(*(model.accessible_attributes.to_a))
   end
 
   def self.passes_attributes
@@ -134,16 +132,16 @@ shared_examples_for "admin resource" do
       page.should have_selector('thead')
       page.should have_selector('tbody')
       page.should have_selector('tfoot')
-      all('thead tr th').size.should eq(model.columns.size - model.columns.find_all {|c| c.type == :binary}.size + 1)
+      all('thead tr th').size.should eq(model.columns(:admin => true).size + 1)
     end
 
     describe 'dataTables' do
       before(:all) do
         (@model = described_class.extract_resource).should be_a_descendant_of(ActiveRecord::Base)
-        @columns = @model.columns.find_all {|c| c.type != :binary}.collect {|c| c.name}
+        @columns = @model.column_names(:admin => true)
         @count = 12
 
-        @count.times do
+        (@count - @model.count).times do
           create(@model.to_s.underscore.to_sym)
         end
 
@@ -173,7 +171,7 @@ shared_examples_for "admin resource" do
         @default_request[:bSearchable_0] = 'false'
         @default_request[('bSortable_' + (columns_num - 1).to_s).to_sym] = 'false'
         @default_request[('bSearchable_' + (columns_num - 1).to_s).to_sym] = 'false'
-        @expected_behaviour = {}
+        @expected_behaviour = {:order => @model.column_names(:admin => true).first.to_s + ' asc'}
       end
 
       before(:each) do
@@ -181,6 +179,10 @@ shared_examples_for "admin resource" do
       end
 
       let(:check_response_for_dataTables) do
+        @count = @model.count
+        iDisplayLength = @params[:iDisplayLength].to_i < 0 ? @count : @params[:iDisplayLength].to_i
+        @expected_behaviour.merge!(:offset => @params[:iDisplayStart], :limit => iDisplayLength)
+
         get :index, @params
         json = parse_json(@response.body)
         expected_count = @model.count(@expected_behaviour)
@@ -189,14 +191,12 @@ shared_examples_for "admin resource" do
         json['iTotalDisplayRecords'].should eq(expected_count)
         json['sEcho'].should eq(@params[:sEcho])
 
-        iDisplayLength = @params[:iDisplayLength].to_i < 0 ? @count : @params[:iDisplayLength].to_i
-
-        results = @model.all(@expected_behaviour.merge(:offset => @params[:iDisplayStart], :limit => iDisplayLength))
+        results = @model.all(@expected_behaviour)
         json['aaData'].should have([ iDisplayLength, results.size ].min ).items
 
         results.each.with_index do |result, r|
           @columns.each.with_index do |column, c|
-            json['aaData'][r][c.to_s].to_s.should eq(result.send(column).to_s)
+            json['aaData'][r][c.to_s].to_s.should eq(AdminBootstrap::DataTable.parse_value(result, column))
           end
           json['aaData'][r]['DT_RowId'].should eq(result.id.to_s)
         end
@@ -211,10 +211,9 @@ shared_examples_for "admin resource" do
         json = parse_json(@response.body)
         last_column_index = @columns.size.to_s
         json['aaData'].each do |result|
-          id = result['0']
-          result[last_column_index].should match(Regexp.new("('|\")/admin/[^\"']+/#{id}/edit('|\")"))
-          result[last_column_index].should match(Regexp.new("('|\")/admin/[^\"']+/#{id}('|\")"))
-          result[last_column_index].should match(/data-method=('|")delete('|")/)
+          result[last_column_index].should match(Regexp.new("('|\")/admin/[^\"']+/[0-9]+/edit('|\")")) unless @model.admin_options[:disabled_actions] and @model.admin_options[:disabled_actions][:value].include?(:edit)
+          result[last_column_index].should match(Regexp.new("('|\")/admin/[^\"']+/[0-9]+('|\")")) unless @model.admin_options[:disabled_actions] and @model.admin_options[:disabled_actions][:value].include?(:show)
+          result[last_column_index].should match(/data-method=('|")delete('|")/) unless @model.admin_options[:disabled_actions] and @model.admin_options[:disabled_actions][:value].include?(:destroy)
           result[last_column_index].should match(/data-toggle=('|")dropdown('|")/)
         end
       end
@@ -240,23 +239,30 @@ shared_examples_for "admin resource" do
       it 'should filter search results' do
         (model = described_class.extract_resource).should be_a_descendant_of(ActiveRecord::Base)
 
-        @params[:sSearch] = search = model.all.collect do |m|
-          column_name = model.columns.find {|c| c.type == :string}.name
-          m.send(column_name) unless m.send(column_name).blank?
-        end.first
+        column = (model.columns(:admin => true) & model.columns).find {|c| c.type == :string or c.type == :text}
 
-        @expected_behaviour = {:conditions => "`" + @columns.join("` LIKE '%#{search}%' OR `") + "` LIKE '%#{search}%'"}
+        if column
+          column_name = column.name
 
-        check_response_for_dataTables
+          @params[:sSearch] = search = model.all.collect do |m|
+            m.send(column_name) unless m.send(column_name).blank?
+          end.first
+
+          @expected_behaviour = {:conditions => "`" + (@columns & @model.column_names).join("` LIKE '%#{search}%' OR `") + "` LIKE '%#{search}%'"}
+
+          check_response_for_dataTables
+        end
       end
 
       it 'should sort by third column ascending and then by first column descending' do
+        admin_columns = @model.column_names(:admin => true)
+        good_columns = admin_columns & @model.column_names
         @params[:iSortingCols] = 2
-        @params[:iSortCol_0] = 2
-        @params[:iSortCol_1] = 0
+        @params[:iSortCol_0] = admin_columns.index(good_columns[2])
+        @params[:iSortCol_1] = admin_columns.index(good_columns[0])
         @params[:sSortDir_0] = 'asc'
         @params[:sSortDir_1] = 'desc'
-        @expected_behaviour = {:order => "`#{@columns[2]}` ASC, `#{@columns[0]}` DESC"}
+        @expected_behaviour = {:order => "`#{good_columns[2]}` ASC, `#{good_columns[0]}` DESC"}
         check_response_for_dataTables
       end
     end
@@ -266,97 +272,107 @@ shared_examples_for "admin resource" do
   context 'with existing resource' do
     before { subject }
 
+    disabled_actions = described_class.extract_resource.admin_options[:disabled_actions] || {:value => []}
+
     let(:resource) do
       (model = described_class.extract_resource).should be_a_descendant_of(ActiveRecord::Base)
       create(model.to_s.underscore.to_sym)
     end
 
-    describe '#show' do
-      subject { get :show, :id => resource.id }
+    unless disabled_actions[:value].include?(:show)
+      describe '#show' do
+        subject { get :show, :id => resource.id }
 
-      renders_show
-      fetches_resource
+        renders_show
+        fetches_resource
+      end
     end
 
-    describe "#edit" do
-      subject { get :edit, :id => resource.id }
-
-      renders_edit
-      fetches_resource
-    end
-
-    describe "#update" do
-      subject { put :update, :id => resource.id, :user => attr }
-
-      context "incorrect data" do
-
-        let(:attr) { incorrect_data }
-
-        fetches_instance
-        passes_attributes
-
-        it "does not save changed instance" do
-          assigns[:resource].changes.should_not be_empty
-        end
+    unless disabled_actions[:value].include?(:edit)
+      describe "#edit" do
+        subject { get :edit, :id => resource.id }
 
         renders_edit
-
+        fetches_resource
       end
+    end
 
-      context "correct data" do
-        let(:attr) { correct_data }
+    unless disabled_actions[:value].include?(:update)
+      describe "#update" do
+        subject { put :update, :id => resource.id, described_class.extract_resource.to_s.underscore.to_sym => attr }
 
-        fetches_instance
-        passes_attributes
+        context "incorrect data" do
 
-        it "saves changed instance" do
-          assigns[:resource].changes.should be_empty
+          let(:attr) { incorrect_data }
+
+          fetches_instance
+          passes_attributes
+
+          it "does not save changed instance" do
+            assigns[:resource].changes.should_not be_empty
+          end
+
+          renders_edit
+
         end
 
-        redirects_to_show
+        context "correct data" do
+          let(:attr) { correct_data }
 
-        it "assigns flash message" do
-          (model = described_class.extract_resource).should be_a_descendant_of(ActiveRecord::Base)
-          flash[:notice].should eq("#{model.to_s} was successfully updated.")
+          fetches_instance
+          passes_attributes
+
+          it "saves changed instance" do
+            assigns[:resource].changes.should be_empty
+          end
+
+          redirects_to_show
+
+          it "assigns flash message" do
+            (model = described_class.extract_resource).should be_a_descendant_of(ActiveRecord::Base)
+            flash[:notice].should eq("#{model.to_s} was successfully updated.")
+          end
         end
       end
     end
 
+    unless disabled_actions[:value].include?(:destroy)
+      describe '#destroy' do
+        render_views
 
-    describe '#destroy' do
-      render_views
-
-      before(:all) do
-        (model = described_class.extract_resource).should be_a_descendant_of(ActiveRecord::Base)
-        model.destroy_all
-      end
-
-      it 'should destroy specified records' do
-        ids = []
-        (model = described_class.extract_resource).should be_a_descendant_of(ActiveRecord::Base)
-        5.times do
-          ids.push create(model.to_s.underscore.to_sym).id
+        before(:all) do
+          (model = described_class.extract_resource).should be_a_descendant_of(ActiveRecord::Base)
+          model.destroy_all
         end
 
-        destroy_ids = ids.shuffle[0..1]
+        it 'should destroy specified records' do
+          ids = []
+          (model = described_class.extract_resource).should be_a_descendant_of(ActiveRecord::Base)
+          5.times do
+            ids.push create(model.to_s.underscore.to_sym).id
+          end
+          size = model.count
 
-        delete :destroy, :id => destroy_ids.join(','), :format => 'json'
+          destroy_ids = ids.shuffle[0..1]
 
-        model.count.should eq(3)
-        model.all.collect(&:id).should_not include(destroy_ids)
-        json = parse_json(@response.body)
-        json['code'].should eq('SUCCESS')
-        json['message'].should_not be_empty
+          delete :destroy, :id => destroy_ids.join(','), :format => 'json'
+
+          model.count.should eq(size - 2)
+          model.all.collect(&:id).should_not include(destroy_ids)
+          json = parse_json(@response.body)
+          json['code'].should eq('SUCCESS')
+          json['message'].should_not be_empty
+        end
+
+        it 'should destroy given record' do
+          rows = resource.class.count
+
+          delete :destroy, :id => resource.id
+
+          resource.class.count.should eq(rows - 1)
+        end
+
       end
-
-      it 'should destroy given record' do
-        rows = resource.class.count
-
-        delete :destroy, :id => resource.id
-
-        resource.class.count.should eq(rows - 1)
-      end
-
     end
 
   end
@@ -364,42 +380,47 @@ shared_examples_for "admin resource" do
   context "without existing resource" do
     before { subject }
 
-    describe "#new" do
-      let(:subject) { get :new }
-      renders_new
-    end
+    disabled_actions = described_class.extract_resource.admin_options[:disabled_actions] || {:value => []}
 
-    describe "#create" do
-      subject { post :create, :user => attr }
-
-      context "incorrect data" do
-        let(:attr) { incorrect_data }
-
-        it "does not new resource" do
-          assigns[:resource].should_not be_persisted
-        end
-
+    unless disabled_actions[:value].include?(:new)
+      describe "#new" do
+        let(:subject) { get :new }
         renders_new
       end
-
-      context "correct data" do
-        let(:attr) { correct_data }
-        passes_attributes
-
-        it "creates new user" do
-          assigns[:resource].should be_persisted
-        end
-
-        redirects_to_show
-
-        it "assigns flash message" do
-          (model = described_class.extract_resource).should be_a_descendant_of(ActiveRecord::Base)
-          flash[:notice].should eq("#{model.to_s} was successfully created!")
-        end
-
-      end
     end
 
+    unless disabled_actions[:value].include?(:create)
+      describe "#create" do
+        subject { post :create, described_class.extract_resource.to_s.underscore.to_sym => attr }
+
+        context "incorrect data" do
+          let(:attr) { incorrect_data }
+
+          it "does not create new resource" do
+            assigns[:resource].should_not be_persisted
+          end
+
+          renders_new
+        end
+
+        context "correct data" do
+          let(:attr) { correct_data }
+          passes_attributes
+
+          it "creates new record" do
+            assigns[:resource].should be_persisted
+          end
+
+          redirects_to_show
+
+          it "assigns flash message" do
+            (model = described_class.extract_resource).should be_a_descendant_of(ActiveRecord::Base)
+            flash[:notice].should eq("#{model.to_s} was successfully created!")
+          end
+
+        end
+      end
+    end
   end
 
 end
