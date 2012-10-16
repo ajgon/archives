76e957dfa36dd658600bb90333eb16c4ecaf38e0
Igor Rzegocki <ajgon@irgon.com>
2012-10-16 23:02:40 +0200

    Added favourites support

--- flickr-photos/photos.js
+++ flickr-photos/photos.js
@@ -1,5 +1,7 @@
 /*global jQuery*/
 
+var FavIds;
+
 var setupPhotos = (function ($) {
     function each (items, callback) {
         var i;
@@ -66,10 +68,55 @@ var setupPhotos = (function ($) {
         return function (img) {
             var elm = document.createElement('div');
             elm.className = 'photo';
+            elm.flickr_id = img.src.match(/\/([0-9]+)_/)[1];
             elm.appendChild(img);
             holder.appendChild(elm);
+            addFavourite(elm);
         };
     }
+
+    function addFavourite( for_elm ) {
+        var fav_btn = document.createElement('a');
+        fav_btn.className = 'favourite ' + (FavIds.get().indexOf(for_elm.flickr_id.toString()) >= 0 ? 'icon-heart' : 'icon-heart-empty');
+        fav_btn.href = '#';
+        fav_btn.toggleFavourite = toggleFavourite.bind(fav_btn);
+        fav_btn.addEventListener('click', function(e) {
+            var item = e.target;
+            e.preventDefault();
+            item.toggleFavourite(item.className.match('icon-heart-empty'));
+        });
+        for_elm.appendChild(fav_btn);
+    }
+
+    function toggleFavourite() {
+        if(this.className.match('icon-heart-empty')) {
+            this.className = this.className.replace('icon-heart-empty', 'icon-heart');
+            FavIds.add(this.parentNode.flickr_id);
+        } else {
+            this.className = this.className.replace('icon-heart', 'icon-heart-empty');
+            FavIds.remove(this.parentNode.flickr_id);
+        }
+    }
+
+    FavIds = {
+        get: function() {
+            var fav_ids = (document.cookie.match(/fav_ids=([0-9,]*)/) || ['', ''])[1].split(','),
+                empty_index = fav_ids.indexOf('');
+            if(empty_index >= 0) { fav_ids.splice(empty_index, 1); }
+            return fav_ids;
+        },
+        add: function(fav_id) {
+            FavIds.remove(fav_id);
+            document.cookie = 'fav_ids=' + FavIds.get().concat([fav_id]).join(',');
+        },
+        remove: function(fav_id) {
+            var tmp = FavIds.get(),
+                fav_id_index = tmp.indexOf(fav_id.toString());
+            if(fav_id_index >= 0) { tmp.splice(fav_id_index, 1); }
+            document.cookie = 'fav_ids=' + tmp.join(',');
+        }
+    }
+
 
     // ----
     
