9392cc6ad90b15ed35eda7db6b4d65203457af21
Igor Rzegocki <ajgon@irgon.com>
2013-02-04 17:08:59 +1100

    jsLinted

--- flexi-carousel/js/carousel.slides.dissolve.js
+++ flexi-carousel/js/carousel.slides.dissolve.js
@@ -1,4 +1,13 @@
-(function($) {
+/*global jQuery */
+/*properties
+ beforeInit, currentIndex, fadeIn, fadeOut, flexiCarousel, fn, getAllSlides,
+ getFlowOptions, getSlide, hide, name, settings, showFrame, switchDuration,
+ type
+ */
+
+(function ($) {
+
+    "use strict";
 
     var slidesDissolve = {
         type: 'slidesFlow',
@@ -6,10 +15,10 @@
         settings: {
             switchDuration: 500
         },
-        beforeInit: function() {
+        beforeInit: function () {
             this.getAllSlides().hide();
         },
-        showFrame: function(frameIndex) {
+        showFrame: function (frameIndex) {
             var options = this.getFlowOptions('slides');
             this.getSlide(this.currentIndex).fadeOut(options.switchDuration);
             this.getSlide(frameIndex).fadeIn(options.switchDuration);
