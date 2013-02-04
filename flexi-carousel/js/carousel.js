9e7258bacaff45f14820332a11ea8769e65ec5c6
Igor Rzegocki <ajgon@irgon.com>
2013-02-04 17:08:59 +1100

    jsLinted

--- flexi-carousel/js/carousel.js
+++ flexi-carousel/js/carousel.js
@@ -1,5 +1,24 @@
-(function($) {
-    var FlexiCarousel = function(items, options) {
+/*global jQuery*/
+/*properties
+ DEFAULTS, addClass, append, apply, call, clickEvent, closest, currentIndex,
+ data, determineFrameIndex, each, elem, empty, error, extend, filter, find,
+ fireCallback, flexiCarousel, fn, getAllSlides, getAllThumbnails,
+ getFlowOptions, getNavItem, getSlide, getThumbnail, hasOwnProperty, hide,
+ index, init, initEvents, items, length, loadModuleOptions, loop, match, name,
+ on, populate, preventDefault, prototype, registerFlow, removeClass, replace,
+ settings, show, showFrame, size, slice, slide, slides, slidesContainer,
+ slidesFlow, slidesFlowOptions, slidesFlows, slidesNext, slidesPrevious,
+ slidesTagName, source, sourceFlow, sourceFlowOptions, sourceFlows,
+ startIndex, successfulSlide, successfulSource, successfulThumbnail,
+ switchFrame, switchSlide, switchThumbnail, thumbnail, thumbnailCurrent,
+ thumbnails, thumbnailsContainer, thumbnailsFlow, thumbnailsFlowOptions,
+ thumbnailsFlows, thumbnailsNext, thumbnailsPrevious, thumbnailsTagName, type
+ */
+
+(function ($) {
+    "use strict";
+
+    var FlexiCarousel = function (items, options) {
         this.settings = $.extend(FlexiCarousel.DEFAULTS, options);
         this.loadModuleOptions(options);
         this.clickEvent = 'click';
@@ -17,140 +36,140 @@
     };
 
     FlexiCarousel.prototype = {
-        loadModuleOptions: function(options) {
+        loadModuleOptions: function (options) {
             var f, fName;
-            for(f in FlexiCarousel.DEFAULTS) {
-                if(f.match(/FlowOptions$/) && FlexiCarousel.DEFAULTS.hasOwnProperty(f)) {
+            for (f in FlexiCarousel.DEFAULTS) {
+                if (FlexiCarousel.DEFAULTS.hasOwnProperty(f) && f.match(/FlowOptions$/)) {
                     fName = f.replace(/Options$/, '');
-                    if(this.settings[fName + 's'][this.settings[fName]]) {
+                    if (this.settings[fName + 's'][this.settings[fName]]) {
                         this.settings[f] = $.extend(this.settings[fName + 's'][this.settings[fName]].settings, options[f]);
                     }
                 }
             }
         },
-        initEvents: function() {
+        initEvents: function () {
             var $fc = this;
-            this.getNavItem('slidesNext').on(this.clickEvent, function(e) {
+            this.getNavItem('slidesNext').on(this.clickEvent, function (e) {
                 e.preventDefault();
                 $fc.showFrame('next');
             });
-            this.getNavItem('slidesPrevious').on(this.clickEvent, function(e) {
+            this.getNavItem('slidesPrevious').on(this.clickEvent, function (e) {
                 e.preventDefault();
                 $fc.showFrame('previous');
             });
-            this.getNavItem('thumbnails').on(this.clickEvent, '.' + this.settings.elem.thumbnail, function(e) {
+            this.getNavItem('thumbnails').on(this.clickEvent, '.' + this.settings.elem.thumbnail, function (e) {
                 var $this = $(this);
                 e.preventDefault();
                 $fc.showFrame($this.closest('.' + $fc.settings.elem.thumbnails).find('.' + $fc.settings.elem.thumbnail).index($this));
             });
         },
 
-        fireCallback: function() {
-            var name = arguments[0],
-                params = Array.prototype.slice.call(arguments, 1),
+        fireCallback: function (name) {
+            var params = Array.prototype.slice.call(arguments, 1),
                 successful = {
                     successfulSlide: false,
                     successfulThumbnail: false,
                     successfulSource: true
                 };
-            if(FlexiCarousel.DEFAULTS.sourceFlows[this.settings.sourceFlow] && FlexiCarousel.DEFAULTS.sourceFlows[this.settings.sourceFlow][name]) {
+            if (FlexiCarousel.DEFAULTS.sourceFlows[this.settings.sourceFlow] && FlexiCarousel.DEFAULTS.sourceFlows[this.settings.sourceFlow][name]) {
                 FlexiCarousel.DEFAULTS.sourceFlows[this.settings.sourceFlow][name].apply(this, params);
             }
-            if(FlexiCarousel.DEFAULTS.slidesFlows[this.settings.slidesFlow] && FlexiCarousel.DEFAULTS.slidesFlows[this.settings.slidesFlow][name]) {
+            if (FlexiCarousel.DEFAULTS.slidesFlows[this.settings.slidesFlow] && FlexiCarousel.DEFAULTS.slidesFlows[this.settings.slidesFlow][name]) {
                 FlexiCarousel.DEFAULTS.slidesFlows[this.settings.slidesFlow][name].apply(this, params);
                 successful.successfulSlide = true;
             }
-            if(FlexiCarousel.DEFAULTS.thumbnailsFlows[this.settings.thumbnailsFlow] && FlexiCarousel.DEFAULTS.thumbnailsFlows[this.settings.thumbnailsFlow][name]) {
+            if (FlexiCarousel.DEFAULTS.thumbnailsFlows[this.settings.thumbnailsFlow] && FlexiCarousel.DEFAULTS.thumbnailsFlows[this.settings.thumbnailsFlow][name]) {
                 FlexiCarousel.DEFAULTS.thumbnailsFlows[this.settings.thumbnailsFlow][name].apply(this, params);
                 successful.successfulThumbnail = true;
             }
             return successful;
         },
-        determineFrameIndex: function(frameIndicator) {
+        determineFrameIndex: function (frameIndicator) {
             var slidesSize = this.getAllSlides().size(),
                 frameIndex = this.currentIndex;
-            switch(frameIndicator) {
-                case 'next':
-                    frameIndex += 1;
-                    break;
-                case 'previous':
-                    frameIndex -= 1;
-                    break;
-                case undefined:
-                    break;
-                default:
-                    frameIndex = parseInt(frameIndicator, 10);
+            switch (frameIndicator) {
+            case 'next':
+                frameIndex += 1;
+                break;
+            case 'previous':
+                frameIndex -= 1;
+                break;
+            case undefined:
+                break;
+            default:
+                frameIndex = parseInt(frameIndicator, 10);
             }
-            if(frameIndex < 0) {
+            if (frameIndex < 0) {
                 frameIndex = this.settings.loop ? slidesSize - 1 : 0;
             }
-            if(frameIndex > slidesSize - 1) {
+            if (frameIndex > slidesSize - 1) {
                 frameIndex = this.settings.loop ? 0 : slidesSize - 1;
             }
             return frameIndex;
         },
-        populate: function(jsonData) {
+        populate: function (jsonData) {
             var slidesContainer = this.items.find('.' + this.settings.elem.slides),
                 thumbnailsContainer = this.items.find('.' + this.settings.elem.thumbnails),
-                jd, jdLen = jsonData.length;
+                jd,
+                jdLen = jsonData.length;
             slidesContainer.empty();
             thumbnailsContainer.empty();
-            for(jd = 0; jd < jdLen; jd++) {
-                slidesContainer.append($('<' + this.settings.elem.slidesTagName + ' class="' + this.settings.elem.slide + '">' + jsonData[jd]['slide'] + '</' + this.settings.elem.slidesTagName + '>'));
-                if(jsonData[jd]['thumbnail']) {
-                    thumbnailsContainer.append($('<' + this.settings.elem.thumbnailsTagName + ' class="' + this.settings.elem.thumbnail + '">' + jsonData[jd]['thumbnail'] + '</' + this.settings.elem.thumbnailsTagName + '>'));
+            for (jd = 0; jd < jdLen; jd += 1) {
+                slidesContainer.append($('<' + this.settings.elem.slidesTagName + ' class="' + this.settings.elem.slide + '">' + jsonData[jd].slide + '</' + this.settings.elem.slidesTagName + '>'));
+                if (jsonData[jd].thumbnail) {
+                    thumbnailsContainer.append($('<' + this.settings.elem.thumbnailsTagName + ' class="' + this.settings.elem.thumbnail + '">' + jsonData[jd].thumbnail + '</' + this.settings.elem.thumbnailsTagName + '>'));
                 }
             }
             this.switchFrame();
         },
 
-        switchSlide: function(slideIndex) {
+        switchSlide: function (slideIndex) {
             this.getAllSlides().hide();
             this.getSlide(slideIndex).show();
         },
-        switchThumbnail: function(thumbnailIndex) {
+        switchThumbnail: function (thumbnailIndex) {
             this.getAllThumbnails().removeClass(this.settings.elem.thumbnailCurrent);
             this.getThumbnail(thumbnailIndex).addClass(this.settings.elem.thumbnailCurrent);
         },
-        switchFrame: function(frameIndicator) {
-            frameIndex = this.determineFrameIndex(frameIndicator);
+        switchFrame: function (frameIndicator) {
+            var frameIndex = this.determineFrameIndex(frameIndicator);
             this.switchSlide(frameIndex);
             this.switchThumbnail(frameIndex);
         },
 
-        showFrame: function(frameIndicator) {
+        showFrame: function (frameIndicator) {
             var successful,
                 frameIndex = this.determineFrameIndex(frameIndicator);
-            if(frameIndex != this.currentIndex) {
+            if (frameIndex !== this.currentIndex) {
                 successful = this.fireCallback('showFrame', frameIndex);
-                if(!successful.successfulSlide) {
+                if (!successful.successfulSlide) {
                     this.switchSlide(frameIndex);
                 }
-                if(!successful.successfulThumbnail) {
+                if (!successful.successfulThumbnail) {
                     this.switchThumbnail(frameIndex);
                 }
                 this.currentIndex = frameIndex;
             }
         },
 
-        getAllSlides: function() {
+        getAllSlides: function () {
             return this.items.find('.' + this.settings.elem.slides + ' > .' + this.settings.elem.slide);
         },
-        getSlide: function(slideIndex) {
+        getSlide: function (slideIndex) {
             slideIndex = slideIndex === undefined ? this.currentIndex : slideIndex;
             return this.getAllSlides().filter(':eq(' + slideIndex + ')');
         },
-        getAllThumbnails: function() {
+        getAllThumbnails: function () {
             return this.items.find('.' + this.settings.elem.thumbnails + ' > .' + this.settings.elem.thumbnail);
         },
-        getThumbnail: function(thumbnailIndex) {
+        getThumbnail: function (thumbnailIndex) {
             thumbnailIndex = thumbnailIndex === undefined ? this.currentIndex : thumbnailIndex;
             return this.getAllThumbnails().filter(':eq(' + thumbnailIndex + ')');
         },
-        getNavItem: function(name) {
+        getNavItem: function (name) {
             return this.items.find('.' + this.settings.elem[name]);
         },
-        getFlowOptions: function(flowName) {
+        getFlowOptions: function (flowName) {
             // FIXME determine slides/thumbnail flow in a better way
             return this.settings[flowName + 'FlowOptions'];
         }
@@ -187,39 +206,36 @@
         }
     };
 
-    window.FlexiCarousel = FlexiCarousel; // FIXME: DEBUG
-
-
-    $.fn.flexiCarousel = function( method ) {
+    $.fn.flexiCarousel = function (method) {
         var methods = {
-            init: function(options) {
-                return this.each(function() {
+            init: function (options) {
+                return this.each(function () {
                     var $this = $(this),
                         flexiCarousel = new FlexiCarousel($this, options);
 
                     $this.data('flexiCarousel', flexiCarousel);
                 });
             },
-            registerFlow: function(flow) {
-                switch(flow.type) {
-                    case 'thumbnailsFlow':
-                    case 'sourceFlow':
-                        break;
-                    default:
-                        flow.type = 'slidesFlow';
+            registerFlow: function (flow) {
+                switch (flow.type) {
+                case 'thumbnailsFlow':
+                case 'sourceFlow':
+                    break;
+                default:
+                    flow.type = 'slidesFlow';
                 }
                 flow.type = flow.type + 's';
                 FlexiCarousel.DEFAULTS[flow.type][flow.name] = flow;
             }
         };
 
-        if ( methods[method] ) {
-            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        } else if ( typeof method === 'object' || ! method ) {
-            return methods.init.apply( this, arguments );
-        } else {
-            $.error( 'Method ' +  method + ' does not exist on jQuery.flexiCarousel' );
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
         }
+        if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        }
+        $.error('Method ' +  method + ' does not exist on jQuery.flexiCarousel');
 
     };
-})(jQuery);
+}(jQuery));
--- flexi-carousel/js/carousel.sources.json.js
+++ flexi-carousel/js/carousel.sources.json.js
@@ -1,4 +1,13 @@
-(function($) {
+/*global jQuery */
+/*properties
+ ajax, ajaxOptions, beforeInit, crossDomain, dataType, flexiCarousel, fn,
+ getFlowOptions, json, name, noop, populate, settings, source, success, type,
+ url
+ */
+
+(function ($) {
+
+    "use strict";
 
     var sourcesJson = {
         type: 'sourceFlow',
@@ -6,37 +15,39 @@
         settings: {
             url: false,
             ajaxOptions: {},
-            json: {}
+            json: []
         },
-        beforeInit: function() {
+        beforeInit: function () {
             var options = this.getFlowOptions('source'),
                 json = options.json,
                 jsonp = false,
                 local = true,
                 successCallback = options.ajaxOptions.success === undefined ? $.noop : options.ajaxOptions.success,
                 $this = this;
-            switch(this.settings.source) {
-                case 'ajax':
-                    local = false;
-                    break;
-                case 'jsonp':
-                    local = false;
-                    jsonp = true;
-                    break;
-                default:
-                    break;
+            switch (this.settings.source) {
+            case 'ajax':
+                local = false;
+                break;
+            case 'jsonp':
+                local = false;
+                jsonp = true;
+                break;
+            default:
+                break;
             }
-            if(!local) {
-                if(!options.ajaxOptions.url) {
+            if (!local) {
+                if (!options.ajaxOptions.url) {
                     options.ajaxOptions.url = options.url;
                 }
                 options.ajaxOptions.dataType = (jsonp ? 'jsonp' : 'json');
                 options.ajaxOptions.crossDomain = jsonp;
-                options.ajaxOptions.success = function(data, textStatus, jqXHR) {
+                options.ajaxOptions.success = function (data, textStatus, jqXHR) {
                     $this.populate(data);
                     successCallback(data, textStatus, jqXHR);
                 };
                 $.ajax(options.ajaxOptions);
+            } else {
+                $this.populate(json);
             }
 
         }
