7056ac5d70ff66814ace536e4cf0e7617899ee2f
Igor Rzegocki <igor.rzegocki@x-team.com>
2013-11-18 14:16:54 +0100

    Code linting

--- mongoose-migrate/lib/migration.js
+++ mongoose-migrate/lib/migration.js
@@ -1,18 +1,18 @@
-
+'use strict';
 /*!
  * migrate - Migration
  * Copyright (c) 2010 TJ Holowaychuk <tj@vision-media.ca>
  * MIT Licensed
  */
 
+function Migration(title, up, down) {
+    this.title = title;
+    this.up = up;
+    this.down = down;
+}
+
 /**
  * Expose `Migration`.
  */
 
 module.exports = Migration;
-
-function Migration(title, up, down) {
-  this.title = title;
-  this.up = up;
-  this.down = down;
-}
