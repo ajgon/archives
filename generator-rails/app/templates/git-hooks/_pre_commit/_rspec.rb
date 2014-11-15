af09089039aee8a204199c3aa7ae8396f41926e6
Igor Rzegocki <ajgon@irgon.com>
2014-11-15 22:41:08 +0100

    Update _rspec.rb

--- generator-rails/app/templates/git-hooks/_pre_commit/_rspec.rb
+++ generator-rails/app/templates/git-hooks/_pre_commit/_rspec.rb
@@ -4,10 +4,14 @@ module Overcommit
       class Rspec < Base
         def run
           files = applicable_files.select { |f| f.match(%r{/app/|/lib/}) }
-          .map { |f| f.sub(%r{/app/|/lib/}, '/spec/').sub(/\.rb$/, '_spec.rb') }
-          result = execute(%W(#{executable} --fail-fast) + files)
+                  .map { |f| f.sub(%r{/app/|/lib/}, '/spec/').sub(/\.rb$/, '_spec.rb') }
+                  .select { |f| File.exist?(f) }
+          return :pass if files.empty?
+          result = execute(
+            %W(#{config['overwritten_executable'] ? config['overwritten_executable'] : executable}) + files
+          )
           return :pass if result.success?
-          [:fail, result.stdout.chomp.split("\n").grep(/rspec/).join("\n")]
+          [:fail, result.stdout.chomp.split("\n").grep(/rspec/).join("\n") + result.stderr.chomp]
         end
       end
     end
